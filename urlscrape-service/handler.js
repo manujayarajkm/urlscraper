2a197b3e7e1f73d65fe7940255ba17d8fbd607a6
Manu Jayaraj <manujayarajkm@gmail.com>
2019-10-15 20:42:13 +0530

    created function to fetch metadata

--- urlscrape-service/handler.js
+++ urlscrape-service/handler.js
@@ -1,60 +1,59 @@
 const cheerio = require('cheerio');
-const request = require('request');
+const axios = require('axios');
 
 module.exports.scraper = async (event, context, callback) => {
-  //make a new request to the URL provided in the HTTP POST request
-  request(req.body.url, (error, responseHtml) => {
-    let resObj = {};
+  context.callbackWaitsForEmptyEventLoop = false;
+  let resObj = {};
 
-    //if there was an error
-    if (error) {
-      return callback(1, 'error retrieving info');
-    }
-
-    //create the cheerio object
-    (resObj = {}),
-      //set a reference to the document that came back
-      ($ = cheerio.load(responseHtml)),
+  await axios(JSON.parse(event.body).url)
+    .then(response => {
+      const html = response.data;
+      const $ = cheerio.load(html);
       //create a reference to the meta elements
       ($title = $('head title').text()),
-      ($desc = $('meta[name="description"]').attr('content')),
-      ($kwd = $('meta[name="keywords"]').attr('content')),
-      ($ogTitle = $('meta[property="og:title"]').attr('content')),
-      ($ogImage = $('meta[property="og:image"]').attr('content')),
-      ($ogkeywords = $('meta[property="og:keywords"]').attr('content')),
-      ($images = $('img'));
+        ($desc = $('meta[name="description"]').attr('content')),
+        ($kwd = $('meta[name="keywords"]').attr('content')),
+        ($ogTitle = $('meta[property="og:title"]').attr('content')),
+        ($ogImage = $('meta[property="og:image"]').attr('content')),
+        ($ogkeywords = $('meta[property="og:keywords"]').attr('content')),
+        ($images = $('img'));
 
-    if ($title) {
-      resObj.title = $title;
-    }
-
-    if ($desc) {
-      resObj.description = $desc;
-    }
-
-    if ($kwd) {
-      resObj.keywords = $kwd;
-    }
-
-    if ($ogImage && $ogImage.length) {
-      resObj.ogImage = $ogImage;
-    }
-
-    if ($ogTitle && $ogTitle.length) {
-      resObj.ogTitle = $ogTitle;
-    }
-
-    if ($ogkeywords && $ogkeywords.length) {
-      resObj.ogkeywords = $ogkeywords;
-    }
-
-    if ($images && $images.length) {
-      resObj.images = [];
-
-      for (var i = 0; i < $images.length; i++) {
-        resObj.images.push($($images[i]).attr('src'));
+      if ($title) {
+        resObj.title = $title;
       }
-    }
-    return callback(0, JSON.stringify(resObj));
-  });
+
+      if ($desc) {
+        resObj.description = $desc;
+      }
+
+      if ($kwd) {
+        resObj.keywords = $kwd;
+      }
+
+      if ($ogImage && $ogImage.length) {
+        resObj.ogImage = $ogImage;
+      }
+
+      if ($ogTitle && $ogTitle.length) {
+        resObj.ogTitle = $ogTitle;
+      }
+
+      if ($ogkeywords && $ogkeywords.length) {
+        resObj.ogkeywords = $ogkeywords;
+      }
+
+      if ($images && $images.length) {
+        resObj.images = [];
+
+        for (var i = 0; i < $images.length; i++) {
+          resObj.images.push($($images[i]).attr('src'));
+        }
+      }
+    })
+    .catch(console.error);
+  let res = {
+    statusCode: 200,
+    body: JSON.stringify(resObj),
+  };
+  return res;
 };
